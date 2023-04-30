58101ceba1376c22d245f079cdb2b676ef09f8a4
natamaz <sidorova87@mail.ru>
2023-04-30 12:26:50 +0300

    м

---
+++ js/test.js
@@ -0,0 +1,69 @@
+
+// let name;
+// name = 'Natalia';
+// alert(name);
+
+
+// let name = 'Natalia';
+// alert(name);
+
+
+//
+// let massage;
+// massage = 'hi';
+// massage = 'hello'
+// alert(massage);
+
+
+
+
+// const myName = 'Natalia';
+// myName = 'Katia'
+
+
+
+// let x = 1;
+// let n = 1.1
+// let y = 'hi'
+// let b = "h1"
+//
+//
+// let checed = true;
+// let noChecked = false;
+
+
+// console.log(typeof '42');
+
+
+// let value = true;
+// alert(typeof  value);
+//
+// value = String(value);
+// alert(typeof  value);
+
+
+// let str = '123';
+// alert(typeof  str);
+//
+//
+// let num = Number(str);
+// alert(typeof num)
+
+
+//
+// let name = 'my' + 'name' + 'Natalia';
+// alert(name)
+//
+// let sum = (1 + 2);
+// alert(sum)
+
+
+// alert('1' + 2)
+// alert(5 + '2')
+
+
+let name = '2';
+let user = '3';
+alert (name + user);
+
+alert(+name + +user);
